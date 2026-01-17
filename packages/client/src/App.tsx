ffebf15fde918566e7be99731e90b58cbb94f0a3
nadmez <nadmez@yahoo.com>
2026-01-17 14:30:08 +0300

    Prettier setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    console.log("Fetching message from server...");
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error("Error fetching message:", err));
-  }, []);
+   useEffect(() => {
+      console.log('Fetching message from server...');
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message))
+         .catch((err) => console.error('Error fetching message:', err));
+   }, []);
 
-  if (message) {
-    return (
-      <div className="p-4">
-        <p className="font-bold text-3xl">{message}</p>
-        <Button className="mt-4" variant="default">
-          Click Me
-        </Button>
-      </div>
-    );
-  }
+   if (message) {
+      return (
+         <div className="p-4">
+            <p className="font-bold text-3xl">{message}</p>
+            <Button className="mt-4" variant="default">
+               Click Me
+            </Button>
+         </div>
+      );
+   }
 
-  return <>No message available.</>;
+   return <>No message available.</>;
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,19 @@
-import express, { type Request, type Response } from "express";
-import dotenv from "dotenv";
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
 const app = express();
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(process.env.OPENAI_API_KEY);
+app.get('/', (req: Request, res: Response) => {
+   res.send(process.env.OPENAI_API_KEY);
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello from the server!" });
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello from the server!' });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+   console.log(`Server listening at http://localhost:${port}`);
 });
