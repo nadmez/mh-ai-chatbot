39cefbf8c2dbdf915b9376b97f086d341a6772dc
nadmez <nadmez@yahoo.com>
2026-01-17 12:13:13 +0300

    Initial server configureation

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(process.env.OPENAI_API_KEY);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
